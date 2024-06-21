0fa75bfd7672bd52d92fe4ecf00e12811940a7bb
Manoj Kumar Munda <manojkrmunda828@gmail.com>
2024-06-21 09:47:13 +0530

    prisma setup && signup route done

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,40 @@
-import { Hono } from 'hono'
+import { PrismaClient } from "@prisma/client/extension";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+  };
+}>();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.post("/api/v1/signup", async(c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
 
-export default app
+  const body = await c.req.json();
+  await prisma.user.create({
+    data: {
+      email: body.email,
+      password: body.password 
+    }
+  })
+
+  return c.text("Hello Hono");
+});
+
+app.post("/api/v1/signin", (c) => {
+  return c.text("Hello Hono");
+});
+app.post("/api/v1/blog", (c) => {
+  return c.text("Hello Hono");
+});
+app.put("/api/v1/blog", (c) => {
+  return c.text("Hello Hono");
+});
+app.get("/api/v1/blog/:id", (c) => {
+  return c.text("Hello Hono");
+});
+
+export default app;
