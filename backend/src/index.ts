e4fb807b8339277fb5d0499a825d2bcbe94fb668
Manoj Kumar Munda <manojkrmunda828@gmail.com>
2024-06-21 10:07:09 +0530

    signup done

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,27 +1,36 @@
 import { PrismaClient } from "@prisma/client/extension";
 import { withAccelerate } from "@prisma/extension-accelerate";
 import { Hono } from "hono";
+import { sign } from "hono/jwt";
 
 const app = new Hono<{
   Bindings: {
     DATABASE_URL: string;
+    JWT_SECRET: string;
   };
 }>();
 
-app.post("/api/v1/signup", async(c) => {
+app.post("/api/v1/signup", async (c) => {
   const prisma = new PrismaClient({
     datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate());
 
   const body = await c.req.json();
-  await prisma.user.create({
-    data: {
-      email: body.email,
-      password: body.password 
-    }
-  })
+  try {
+    const user = await prisma.user.create({
+      data: {
+        email: body.email,
+        password: body.password,
+      },
+    });
 
-  return c.text("Hello Hono");
+    const jwt = sign({ id: user.id }, c.env.JWT_SECRET);
+
+    return c.json({ jwt });
+  } catch (e) {
+    c.status(403);
+    return c.json({ error: "error while signing up" });
+  }
 });
 
 app.post("/api/v1/signin", (c) => {
